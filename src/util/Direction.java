aefe8a1b14e7266092b19601c103fb11b9cce8dc
Thyron-Manamtam <tgmanamtam@up.edu.ph>
2026-04-23 17:44:24 +0800

    feat(util): add Direction enum with dRow/dCol deltas and opposite()

---
+++ src/util/Direction.java
@@ -0,0 +1,33 @@
+package util;
+
+public enum Direction {
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    NONE(0, 0);
+
+    private final int dRow, dCol;
+
+    Direction(int dRow, int dCol) {
+        this.dRow = dRow;
+        this.dCol = dCol;
+    }
+
+    public int getDRow() { return dRow; }
+    public int getDCol() { return dCol; }
+
+    public int[] toDelta() {
+        return new int[]{ dRow, dCol };
+    }
+
+    public Direction opposite() {
+        switch (this) {
+            case UP:    return DOWN;
+            case DOWN:  return UP;
+            case LEFT:  return RIGHT;
+            case RIGHT: return LEFT;
+            default:    return NONE;
+        }
+    }
+}
