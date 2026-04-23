b2b46b02e7b6bd69ec14519e0d8b8d1c423c2b69
Thyron-Manamtam <tgmanamtam@up.edu.ph>
2026-04-23 17:44:51 +0800

    feat(util): add GameState enum for game loop state machine

---
+++ src/util/GameState.java
@@ -0,0 +1,10 @@
+package util;
+
+public enum GameState {
+    START,      // Title/start screen
+    PLAYING,    // Active gameplay
+    PAUSED,     // Paused
+    DEAD,       // Brief death animation
+    GAME_OVER,  // All lives lost
+    WIN         // All pellets collected
+}
