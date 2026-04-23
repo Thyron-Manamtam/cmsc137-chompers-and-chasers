4f4288c47680f603efd7475aea38010b8c234084
Thyron-Manamtam <tgmanamtam@up.edu.ph>
2026-04-23 17:46:14 +0800

    feat(model): add Pellet with NORMAL and POWER types and collected state

---
+++ src/model/Pellet.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Pellet {
+    public enum Type { NORMAL, POWER }
+
+    private final int row, col;
+    private final Type type;
+    private boolean collected;
+
+    public Pellet(int row, int col, Type type) {
+        this.row = row;
+        this.col = col;
+        this.type = type;
+        this.collected = false;
+    }
+
+    public void collect()           { this.collected = true; }
+    public void reset()             { this.collected = false; }
+
+    public boolean isCollected()    { return collected; }
+    public boolean isPower()        { return type == Type.POWER; }
+    public int getRow()             { return row; }
+    public int getCol()             { return col; }
+    public Type getType()           { return type; }
+}
