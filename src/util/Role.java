cc6bd1dd7a1d550817bbd556d8a6c99e6fa8b36d
Thyron-Manamtam <tgmanamtam@up.edu.ph>
2026-04-23 17:45:16 +0800

    feat(util): add Role enum for CHOMPER and CHASER player roles

---
+++ src/util/Role.java
@@ -0,0 +1,12 @@
+package util;
+
+/**
+ * Player roles as defined in the project specification.
+ * Chompers collect pellets; Chasers attempt to catch Chompers.
+ *
+ * Milestone 2: roles will be assigned by the server upon connection.
+ */
+public enum Role {
+    CHOMPER,
+    CHASER
+}
